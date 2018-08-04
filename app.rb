49b952464864417ff80a8a4cf1703732e7c2cba5
Lorena <lorena.giddens@outlook.com>
2018-08-04 23:30:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
+=begin
+
+Application Controller
+handles all incoming requests to the app,
+and sends back the appropriate responses to the client.
+
+=end
+
 require 'sinatra'
 class App < Sinatra::Base
 
-  get '/' do 
+  get '/' do
     "Hello, world!"
   end
 
